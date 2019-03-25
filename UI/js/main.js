c5571c36448629ed1d072c84b9aed421e412ae04
timi-codes <tejumoladavid@gmail.com>
2019-03-25 13:39:20 +0100

    [164846790 chore] create account signup modal

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,2 +1,17 @@
 var height = document.getElementById("test").clientHeight;
 document.getElementById("test").style.marginTop = -height / 2 + "px";
+
+var signupModal = document.getElementById("signupModal");
+var loginModal = document.getElementById("loginModal");
+
+var signup = document.getElementById("signup-button");
+var login = document.getElementById("login-button");
+
+signup.onclick = e => {
+  signupModal.style.display = "block";
+};
+
+signupModal.onclick = function(e) {
+  if (e.target != this) return;
+  signupModal.style.display = "none";
+};
