e8cf5cb2444273e2129efeeb92e43a23cb97f75d
timi-codes <tejumoladavid@gmail.com>
2019-03-26 18:06:48 +0100

    [#164846920 Feature] create staff and admin dashboard

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -1,14 +1,15 @@
 var accountModal = document.getElementById("accountModal");
 var account = document.getElementById("account-button");
-var body = document.getElementsByTagName("body")[0];
 
-account.onclick = e => {
-  accountModal.style.display = "block";
-  body.classList.add("stop-scrolling");
-};
+var body = document.getElementsByTagName("body")[0];
 
 accountModal.onclick = function(e) {
   if (e.target != this) return;
   accountModal.style.display = "none";
   body.classList.remove("stop-scrolling");
+};
+
+account.onclick = e => {
+  accountModal.style.display = "block";
+  body.classList.add("stop-scrolling");
 };
