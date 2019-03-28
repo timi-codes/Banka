20ae31e09e1dcbfd5f0f1ee555e71855deb25e01
timi-codes <tejumoladavid@gmail.com>
2019-03-29 00:26:50 +0100

    [#164973542 Feature] add user logout and profile update

--- UI/js/customer.js
+++ UI/js/customer.js
@@ -1,5 +1,6 @@
 var accountModal = document.getElementById("accountModal");
 var body = document.getElementsByTagName("body")[0];
+var profile = document.getElementById("profile-button");
 
 accountModal.onclick = function(e) {
   if (e.target != this) return;
@@ -11,5 +12,16 @@ var credit = document.getElementById("credit-button");
 
 credit.onclick = e => {
   accountModal.style.display = "block";
+  body.classList.add("stop-scrolling");
+};
+
+profileModal.onclick = function(e) {
+  if (e.target != this) return;
+  profileModal.style.display = "none";
+  body.classList.remove("stop-scrolling");
+};
+
+profile.onclick = e => {
+  profileModal.style.display = "block";
   body.classList.add("stop-scrolling");
 };
