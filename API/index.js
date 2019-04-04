c9ff8f9f1ab8e78ab2cbe0e1ea71cd2ca371f83d
timi-codes <tejumoladavid@gmail.com>
2019-04-04 14:28:39 +0100

    chore(eslint):setup eslint with airbnbstyle giude [Finishes #165120756]

--- API/index.js
+++ API/index.js
@@ -1,17 +1,18 @@
-import express from "express";
-import { json } from "body-parser";
+import express from 'express';
+import { json } from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 7888;
 
 app.use(json());
 
-app.get("/", (req, res) => {
-  res.send("This app server is running on it own 😀😎😋");
+app.get('/', (req, res) => {
+  res.send('This app server is running on it own 😀😎😋');
 });
 
 if (!module.parent) {
   app.listen(port, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server is running on port ${port}`);
   });
 }
