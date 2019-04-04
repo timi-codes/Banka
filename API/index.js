78163221e88ad49fcf5458ebe55fcf81f82a9a8f
timi-codes <tejumoladavid@gmail.com>
2019-04-04 11:16:35 +0100

    chore(server):setup a basic server [Start #165116886]

---
+++ API/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { json } from "body-parser";
+
+const app = express();
+const port = process.env.PORT || 7888;
+
+app.use(json());
+
+app.get("/", (req, res) => {
+  res.send("This app server is running on it own 😀😎😋");
+});
+
+if (!module.parent) {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
