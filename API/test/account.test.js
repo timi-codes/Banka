8be5946fc0f6ceac148da60d844aab0a8477dc7d
timi-codes <tejumoladavid@gmail.com>
2019-04-05 11:54:37 +0100

    feature(test):implement unit testing for getting all bank accounts [Finishes #165145115]

--- API/test/account.test.js
+++ API/test/account.test.js
@@ -33,46 +33,63 @@ describe('Test account related endpoints - POST, GET, PATH, DELETE', () => {
   /**
    * Test the POST /accounts/ endpoint
    */
-  describe('POST /accounts', () => {});
+  describe('POST /accounts', () => {
+    it('it should create a bank account', (done) => {
+      const details = {
+        type: 'savings',
+        openingBalance: '0',
+      };
 
-  it('it should create a bank account', (done) => {
-    const details = {
-      type: 'savings',
-      openingBalance: '0',
-    };
+      chai
+        .request(app)
+        .post('/api/v1/accounts')
+        .send(details)
+        .set('x-access-token', generatedToken)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.data.should.have.property('accountNumber');
+          res.body.data.should.have.property('firstName');
+          res.body.data.should.have.property('lastName');
+          res.body.data.should.have.property('email');
+          res.body.data.should.have.property('type');
+          res.body.data.should.have.property('openingBalance');
+          done();
+        });
+    });
 
-    chai
-      .request(app)
-      .post('/api/v1/accounts')
-      .send(details)
-      .set('x-access-token', generatedToken)
-      .end((err, res) => {
-        res.should.have.status(201);
-        res.body.should.be.a('object');
-        res.body.data.should.have.property('accountNumber');
-        res.body.data.should.have.property('firstName');
-        res.body.data.should.have.property('lastName');
-        res.body.data.should.have.property('email');
-        res.body.data.should.have.property('type');
-        res.body.data.should.have.property('openingBalance');
-        done();
-      });
+    it('it should throw error when there is a missing payload fields', (done) => {
+      const details = {
+        type: 'savings',
+      };
+
+      chai
+        .request(app)
+        .post('/api/v1/accounts')
+        .send(details)
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.be.a('object');
+          res.body.data.should.have.property('error');
+          done();
+        });
+    });
   });
 
-  it('it should throw error when there is a missing payload fields', (done) => {
-    const details = {
-      type: 'savings',
-    };
-
-    chai
-      .request(app)
-      .post('/api/v1/accounts')
-      .send(details)
-      .end((err, res) => {
-        res.should.have.status(422);
-        res.body.should.be.a('object');
-        res.body.data.should.have.property('error');
-        done();
-      });
+  /**
+   * Test the GET /accounts/ routes
+   */
+  describe('GET /accounts', () => {
+    it('it should get all the bank accounts', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/accounts')
+        .set('x-access-token', generatedToken)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.data.should.be.a('array');
+          done();
+        });
+    });
   });
 });
