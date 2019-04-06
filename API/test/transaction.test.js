cd42ddf98c1942bc077907a91e2d886983694298
timi-codes <tejumoladavid@gmail.com>
2019-04-06 02:04:46 +0100

    feature(test):implement test cases to credit user account [Finishes #165162441]

--- API/test/transaction.test.js
+++ API/test/transaction.test.js
@@ -91,12 +91,90 @@ describe('Test account related endpoints - POST, GET, PATH, DELETE', () => {
         });
     });
 
-    it('it should throw an error when "status" in request body is invalid ', (done) => {
+    it('it should throw an error when "amount" in request body is invalid ', (done) => {
       const accountNumber = 0o222010772; // octal number format
       const body = {};
       chai
         .request(app)
         .post(`/api/v1/transactions/${accountNumber}/debit`)
+        .set('x-access-token', generatedToken)
+        .send(body)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error').eql('Require request body field "amount"');
+          done();
+        });
+    });
+  });
+
+  /**
+   * Test the POST /transactions/:accountNumber/credit route
+   */
+  describe('POST /transactions/:accountNumber/credit', () => {
+    it('it should credit a bank account', (done) => {
+      const accountNumber = 0o222010772; // octal number format
+      const body = { amount: '50000' };
+      chai
+        .request(app)
+        .post(`/api/v1/transactions/${accountNumber}/credit`)
+        .set('x-access-token', generatedToken)
+        .send(body)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.data.should.have.property('accountNumber').eql(accountNumber);
+          res.body.data.should.have.property('transactionId');
+          res.body.data.should.have.property('amount').eql(body.amount);
+          res.body.data.should.have.property('cashier');
+          res.body.data.should.have.property('transactionType');
+          res.body.data.should.have.property('accountBalance');
+          done();
+        });
+    });
+
+    it('it should throw an error when account number is not found', (done) => {
+      const accountNumber = 0o002020; // octal number format
+      const body = { amount: '50000' };
+
+      chai
+        .request(app)
+        .post(`/api/v1/transactions/${accountNumber}/credit`)
+        .set('x-access-token', generatedToken)
+        .send(body)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Account number cannot be found');
+          done();
+        });
+    });
+
+    it('it should throw an error when account number is invalid', (done) => {
+      const accountNumber = '0o002020fc'; // octal number format
+      const body = { amount: '50000' };
+
+      chai
+        .request(app)
+        .post(`/api/v1/transactions/${accountNumber}/credit`)
+        .set('x-access-token', generatedToken)
+        .send(body)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          res.body.should.have
+            .property('error')
+            .eql('Invalid account number. Account number must be a number');
+          done();
+        });
+    });
+
+    it('it should throw an error when "amount" in request body is invalid ', (done) => {
+      const accountNumber = 0o222010772; // octal number format
+      const body = {};
+      chai
+        .request(app)
+        .post(`/api/v1/transactions/${accountNumber}/credit`)
         .set('x-access-token', generatedToken)
         .send(body)
         .end((err, res) => {
