7695d50cf291c3763d32f1a40eda988995e1a17c
timi-codes <tejumoladavid@gmail.com>
2019-04-16 22:40:44 +0100

    chore(feedback):refactor ui code base to use const and arrow functions [Finishes #165380759 #165380744]

--- UI/js/customer.js
+++ UI/js/customer.js
@@ -1,27 +1,15 @@
-var accountModal = document.getElementById("accountModal");
-var body = document.getElementsByTagName("body")[0];
-var profile = document.getElementById("profile-button");
+const accountModal = document.getElementById('accountModal');
+const body = document.getElementsByTagName('body')[0];
 
-accountModal.onclick = function(e) {
-  if (e.target != this) return;
-  accountModal.style.display = "none";
-  body.classList.remove("stop-scrolling");
+accountModal.onclick = (e) => {
+  if (e.target !== this) return;
+  accountModal.style.display = 'none';
+  body.classList.remove('stop-scrolling');
 };
 
-var credit = document.getElementById("credit-button");
+const credit = document.getElementById('credit-button');
 
-credit.onclick = e => {
-  accountModal.style.display = "block";
-  body.classList.add("stop-scrolling");
-};
-
-profileModal.onclick = function(e) {
-  if (e.target != this) return;
-  profileModal.style.display = "none";
-  body.classList.remove("stop-scrolling");
-};
-
-profile.onclick = e => {
-  profileModal.style.display = "block";
-  body.classList.add("stop-scrolling");
+credit.onclick = () => {
+  accountModal.style.display = 'block';
+  body.classList.add('stop-scrolling');
 };
