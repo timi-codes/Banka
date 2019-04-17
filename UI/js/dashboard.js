c43b7281267828eb44cb11fa6a4605fa9ecfe620
timi-codes <tejumoladavid@gmail.com>
2019-04-17 13:19:56 +0100

    chore(feedback):implement all ui feedbacks

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -1,28 +1,28 @@
-var accountModal = document.getElementById("accountModal");
-var account = document.getElementById("account-button");
-var profileModal = document.getElementById("profileModal");
-var profile = document.getElementById("profile-button");
+const accountModal = document.getElementById('accountModal');
+const account = document.getElementById('account-button');
+const profileModal = document.getElementById('profileModal');
+const profile = document.getElementById('profile-button');
 
-var body = document.getElementsByTagName("body")[0];
+const body = document.getElementsByTagName('body')[0];
 
-profileModal.onclick = function(e) {
-  if (e.target != this) return;
-  profileModal.style.display = "none";
-  body.classList.remove("stop-scrolling");
+profileModal.onclick = (e) => {
+  if (e.target !== e.currentTarget) return;
+  profileModal.style.display = 'none';
+  body.classList.remove('stop-scrolling');
 };
 
-profile.onclick = e => {
-  profileModal.style.display = "block";
-  body.classList.add("stop-scrolling");
+profile.onclick = () => {
+  profileModal.style.display = 'flex';
+  body.classList.add('stop-scrolling');
 };
 
-accountModal.onclick = function(e) {
-  if (e.target != this) return;
-  accountModal.style.display = "none";
-  body.classList.remove("stop-scrolling");
+accountModal.onclick = (e) => {
+  if (e.target !== e.currentTarget) return;
+  accountModal.style.display = 'none';
+  body.classList.remove('stop-scrolling');
 };
 
-account.onclick = e => {
-  accountModal.style.display = "block";
-  body.classList.add("stop-scrolling");
+account.onclick = () => {
+  accountModal.style.display = 'flex';
+  body.classList.add('stop-scrolling');
 };
--- UI/js/main.js
+++ UI/js/main.js
@@ -1,58 +1,59 @@
-var height = document.getElementById("test").clientHeight;
-document.getElementById("test").style.marginTop = -height / 2 + "px";
+const height = document.getElementById('test').clientHeight;
+document.getElementById('test').style.marginTop = `${-height / 2}px`;
 
-var signupModal = document.getElementById("signupModal");
-var loginModal = document.getElementById("loginModal");
+const signupModal = document.getElementById('signupModal');
+const loginModal = document.getElementById('loginModal');
 
-var signup = document.querySelectorAll(".signup-button");
-var login = document.getElementById("login-button");
+const signup = document.querySelectorAll('.signup-button');
+const login = document.getElementById('login-button');
 
-var body = document.getElementsByTagName("body")[0];
+const body = document.getElementsByTagName('body')[0];
 
-console.log(signup)
+console.log(login);
 
-for (button = 0; button < signup.length; button++) {
-  signup[button].onclick = e => {
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-    signupModal.style.display = "block";
-    body.classList.add("stop-scrolling");
+for (let button = 0; button < signup.length; button += 1) {
+  signup[button].onclick = () => {
+    document.body.scrollTop = 0;
+    signupModal.style.display = 'block';
+    body.classList.add('stop-scrolling');
   };
 }
 
-login.onclick = e => {
-  loginModal.style.display = "block";
-  body.classList.add("stop-scrolling");
+login.onclick = () => {
+  loginModal.style.display = 'block';
+  body.classList.add('stop-scrolling');
 };
 
-signupModal.onclick = function(e) {
-  if (e.target != this) return;
-  signupModal.style.display = "none";
-  body.classList.remove("stop-scrolling");
+signupModal.onclick = (e) => {
+  if (e.target !== e.currentTarget) return;
+  signupModal.style.display = 'none';
+  body.classList.remove('stop-scrolling');
 };
 
-loginModal.onclick = function(e) {
-  if (e.target != this) return;
-  loginModal.style.display = "none";
-  body.classList.remove("stop-scrolling");
+
+loginModal.onclick = (e) => {
+  if (e.target !== e.currentTarget) return;
+  loginModal.style.display = 'none';
+  body.classList.remove('stop-scrolling');
 };
 
-var userLogin = document.getElementById("user-login-btn");
-var userRole = document.getElementById("user-role");
-var role = "customer";
-userRole.onchange = function() {
-  role = document.getElementById("user-role").value;
-};
-
-userLogin.onclick = function(e) {
-  gotoNextPage();
+const userLogin = document.getElementById('user-login-btn');
+const userRole = document.getElementById('user-role');
+let role = 'customer';
+userRole.onchange = () => {
+  role = document.getElementById('user-role').value;
 };
 
 function gotoNextPage() {
-  if (role == "customer") {
-    window.location.href = "./customer/dashboard.html";
-  } else if (role == "staff") {
-    window.location.href = "./cashier/dashboard.html";
-  } else if (role == "admin") {
-    window.location.href = "./admin/dashboard.html";
+  if (role === 'customer') {
+    window.location.href = './customer/dashboard.html';
+  } else if (role === 'staff') {
+    window.location.href = './cashier/dashboard.html';
+  } else if (role === 'admin') {
+    window.location.href = './admin/dashboard.html';
   }
 }
+
+userLogin.onclick = () => {
+  gotoNextPage();
+};
