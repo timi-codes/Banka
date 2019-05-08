6a840ed747a627e9f71704857a5d8ea6bc6750a4
timi-codes <tejumoladavid@gmail.com>
2019-05-08 16:22:25 +0100

    Empty commit

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -411,8 +411,6 @@ const paramsEmail = window.location.href.slice(
 const paramsaccountUrl = `${baseUrl}/user/${paramsEmail}/accounts`;
 
 console.log(accountUrl);
-console.log();
-
 if ("/UI/customer/dashboard.html" === window.location.pathname) {
   fetchUserAccount(accountUrl);
 } else if ("/UI/cashier/dashboard.html" === window.location.pathname) {
