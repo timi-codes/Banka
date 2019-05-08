aa1c887ea69d0f665659eea4a6d5aa293e9f1d51
timi-codes <tejumoladavid@gmail.com>
2019-05-08 16:36:21 +0100

    ft(frontend):connect app to api endpoints

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -410,7 +410,7 @@ const paramsEmail = window.location.href.slice(
 
 const paramsaccountUrl = `${baseUrl}/user/${paramsEmail}/accounts`;
 
-console.log(accountUrl);
+console.log(accountUrl + " called");
 // if ("/UI/customer/dashboard.html" === window.location.pathname) {
 fetchUserAccount(accountUrl);
 // } else if ("/UI/cashier/dashboard.html" === window.location.pathname) {
