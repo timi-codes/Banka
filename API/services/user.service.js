177983af3aa105daeef8736b63a241617ad064a6
tejumola <tejumoladavid@gmail.com>
2019-08-28 10:20:57 +0100

    fix

--- API/services/user.service.js
+++ API/services/user.service.js
@@ -1,8 +1,8 @@
-import Utils from '../utils/common';
-import UserModel from '../models/user.model';
-import mailer from '../utils/mailer';
+import Utils from "../utils/common";
+import UserModel from "../models/user.model";
+import mailer from "../utils/mailer";
 
-const User = new UserModel('users');
+const User = new UserModel("users");
 
 /** Class that allows user create and login  */
 class UserService {
@@ -15,23 +15,24 @@ class UserService {
       const isUser = await User.findUserByEmail(user.email);
 
       if (isUser) {
-        throw new Error('a user with this email address already exist');
+        throw new Error("a user with this email address already exist");
       }
 
       const newUser = user;
-      newUser.type = 'client';
+      newUser.type = "client";
       newUser.isAdmin = false;
       newUser.password = Utils.hashPassword(user.password);
       const createdUser = await User.createUser(newUser);
 
-      const {
-        id, type, isadmin, firstname, lastname, email,
-      } = createdUser;
+      const { id, type, isadmin, firstname, lastname, email } = createdUser;
 
       const payload = {
         id,
         type,
         isAdmin: isadmin,
+        firstname,
+        lastname,
+        email,
       };
 
       const token = Utils.jwtSigner(payload);
@@ -60,10 +61,18 @@ class UserService {
       const user = await User.findUserByEmail(login.email);
 
       if (user) {
-        const bycrptResponse = Utils.validatePassword(login.password, user.password);
+        const bycrptResponse = Utils.validatePassword(
+          login.password,
+          user.password,
+        );
         if (bycrptResponse) {
           const {
-            id, firstname, lastname, isadmin, password: userPassword, ...data
+            id,
+            firstname,
+            lastname,
+            isadmin,
+            password: userPassword,
+            ...data
           } = user;
           const userProfile = {
             id,
@@ -81,7 +90,7 @@ class UserService {
           };
         }
       }
-      throw new Error('invalid user credentials');
+      throw new Error("invalid user credentials");
     } catch (err) {
       throw err;
     }
@@ -96,22 +105,21 @@ class UserService {
       const isStaff = await User.findUserByEmail(user.email);
 
       if (isStaff) {
-        throw new Error('a user with this email address already exist');
+        throw new Error("a user with this email address already exist");
       }
 
       const newStaff = user;
-      newStaff.type = 'staff';
+      newStaff.type = "staff";
       const plainPassword = user.password;
       newStaff.password = Utils.hashPassword(user.password);
       const createdStaff = await User.createUser(newStaff);
 
-      const {
-        id, type, isadmin, firstname, lastname, email,
-      } = createdStaff;
+      const { id, type, isadmin, firstname, lastname, email } = createdStaff;
 
       const mailData = {
-        subject: 'An account has created for you on Banka💸💵🏦',
-        text: 'Kindly use the credentials in this mail to login to your account',
+        subject: "An account has created for you on Banka💸💵🏦",
+        text:
+          "Kindly use the credentials in this mail to login to your account",
         to: email,
         html: `<b>Email Adress: ${email}<br/><br/>
           Password: ${plainPassword}<br/><br/>
